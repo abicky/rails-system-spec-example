2eb99ef7e2e685e9c14e0d72ce758131795e971e
abicky <takeshi.arabiki@gmail.com>
2019-09-17 05:01:35 +0900

    Fix the bug that screenshots are not taken correctly

--- spec/support/system_helper.rb
+++ spec/support/system_helper.rb
@@ -1,4 +1,14 @@
 module SystemHelper
+  extend ActiveSupport::Concern
+
+  included do |example_group|
+    # Screenshots are not taken correctly
+    # because RSpec::Rails::SystemExampleGroup calls after_teardown before before_teardown
+    example_group.after do
+      take_failed_screenshot
+    end
+  end
+
   def display_console_logs
     console_logs.each do |log|
       t = log.timestamp
@@ -12,5 +22,11 @@ module SystemHelper
   def console_logs
     @console_logs ||= []
     @console_logs.concat(page.driver.browser.manage.logs.get(:browser))
+  end
+
+  def take_failed_screenshot
+    return if @is_failed_screenshot_taken
+    super
+    @is_failed_screenshot_taken = true
   end
 end
